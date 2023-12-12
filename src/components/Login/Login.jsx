b1e7a81dff3e9aef5920ed01f1d75df9446364a9
JEEL RUPARELIYA <jeelrupareliya255@gmail.com>
2023-12-12 23:21:44 +0530

    new changes in validation of login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,40 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 function Login() {
     const usernameRef = useRef(null);
     const emailRef = useRef(null);
     const password1Ref = useRef(null);
     const password2Ref = useRef(null);
+    const colorRef = useRef(null);
+
+
+    const success = () => {
+        let formcom = document.getElementsByClassName('form-control');
+    }
 
     const setSuccess = (ref) => {
-        // Implement your success logic (e.g., update styles)
         const formControl = ref.current.parentElement;
         formControl.className = "form-control success"
+        ref.current.nextSibling.innerText = '';
+        ref.current.classList.add('border-green-500', 'border-4');
+        ref.current.classList.remove('border-red-500', 'border-4');
+        const small = formControl.querySelector('small');
+        small.innerText = '';
     };
 
     const setError = (input, errorMsg) => {
-        const formControl = input.parentElement;
+        const formControl = input.current.parentElement;
         const small = formControl.querySelector('small');
-        formControl.className = "form-control error"; // Fix this line
+        small.style.color = 'red';
+        small.style.fontWeight = 'bold';
+        formControl.className = "form-control error";
         small.innerText = errorMsg;
+        input.current.classList.add('border-red-500', 'border-4');
     };
 
     const isEmail = (emailValue) => {
         // Your existing email validation logic
-        var atSymbol = emailValue.indexOf('@');
+        const atSymbol = emailValue.indexOf('@');
         if (atSymbol < 1) return false;
         var dot = emailValue.lastIndexOf('.');
         if (dot <= atSymbol + 2) return false;
@@ -38,40 +51,47 @@ function Login() {
 
         // Your existing validation logic
         if (usernameValue === '') {
-            setError(usernameRef, 'Username cannot be null');
+            setError(usernameRef, '*Username cannot be null');
         } else if (usernameValue.length <= 4) {
-            setError(usernameRef, 'Username must be greater than 3');
+            setError(usernameRef, '*Username must be greater than 4');
         } else {
             setSuccess(usernameRef);
         }
 
         // Example for email validation
         if (emailValue === '') {
-            setError(emailRef, 'Email cannot be null');
+            setError(emailRef, '*Email cannot be null');
         } else if (!isEmail(emailValue)) {
-            setError(emailRef, 'Not a valid email');
+            setError(emailRef, '*Not a valid email');
         } else {
             setSuccess(emailRef);
         }
 
         if (password1Value === '') {
-            setError(password1Ref, 'password cannot be null');
+            setError(password1Ref, '*password cannot be null');
         } else if (password1Value.length <= 5) {
-            setError(password1Ref, 'minimum 6 char');
+            setError(password1Ref, '*minimum 6 char');
         } else {
             setSuccess(password1Ref);
         }
 
         if (password2Value === '') {
-            setError(password2Ref, 'confirm password cannot be null');
-        } else if (password1Value !== password2Value) {
-            setError(password2Ref, 'passwords are not matching'); {/* Fix this line */ }
-        } else {
-            setSuccess(password2Ref); {/* Fix this line */ }
+            setError(password2Ref, '*confirm password cannot be null');
+
+        }
+        else if (password2Value.length <= 5) {
+            setError(password2Ref, '*minimum 6 char');
+        }
+        else if (password1Value !== password2Value) {
+            setError(password2Ref, '*passwords are not matching');
+        }
+        else {
+            setSuccess(password2Ref); 
         }
 
-        // Repeat for other fields...
     };
+
+    success();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -84,60 +104,71 @@ function Login() {
             <div className="bg-gray-100 flex items-center justify-center h-screen">
                 <div className="bg-white p-8 rounded shadow-md w-96">
                     <form onSubmit={ handleSubmit } id='form'>
+
+
                         <div className="form-control">
-                            <label htmlFor="username" className="block text-sm font-bold text-gray-700">Username:</label>
+                            <label htmlFor="username" className="mb-2 block text-sm font-bold text-gray-700">Username:</label>
                             <input
                                 type="text"
                                 id="username"
                                 name="username"
                                 placeholder="Enter your username"
-                                required
-                                className="w-full px-3 py-2 mb-4 border rounded-md"
+
+                                className="w-full px-3 py-2 mb-1 border-black border-2 rounded-md"
                                 ref={ usernameRef }
                             />
-                            <small></small>
+                            <div className='mb-6'>
+                                <small ref={ colorRef }
+                                ></small>
+                            </div>
                         </div>
 
                         <div className="form-control">
-                            <label htmlFor="email" className="block text-sm font-bold text-gray-700">Email:</label>
+                            <label htmlFor="email" className="mb-2 block text-sm font-bold text-gray-700">Email:</label>
                             <input
                                 type="email"
                                 id="email"
                                 name="email"
                                 placeholder="Enter your email"
-                                required
-                                className="w-full px-3 py-2 mb-4 border rounded-md"
+
+                                className="w-full px-3 py-2 mb-1 border-black border-2  rounded-md"
                                 ref={ emailRef }
                             />
-                            <small></small>
+                            <div className='mb-6'>
+                                <small ref={ colorRef }></small>
+                            </div>
                         </div>
 
                         <div className="form-control">
-                            <label htmlFor="password" className="block text-sm font-bold text-gray-700">Password:</label>
+                            <label htmlFor="password" className="mb-2 block text-sm font-bold text-gray-700">Password:</label>
                             <input
                                 type="password"
                                 id="password"
                                 name="password"
                                 placeholder="Enter your password"
-                                required
-                                className="w-full px-3 py-2 mb-4 border rounded-md"
+
+                                className="w-full px-3 py-2 mb-1 border-black border-2  rounded-md"
                                 ref={ password1Ref }
                             />
-                            <small></small>
+                            <div className='mb-6'>
+                                <small ref={ colorRef }></small>
+                            </div>
                         </div>
 
                         <div className="form-control">
-                            <label htmlFor="confirmPassword" className="block text-sm font-bold text-gray-700">Confirm Password:</label>
+                            <label htmlFor="confirmPassword" className="mb-2 block text-sm font-bold text-gray-700">Confirm Password:</label>
                             <input
                                 type="password"
                                 id="confirmPassword"
                                 name="confirmPassword"
                                 placeholder="Confirm your password"
-                                required
-                                className="w-full px-3 py-2 mb-4 border rounded-md"
+
+                                className="w-full px-3 py-2 mb-1 border-black border-2  rounded-md"
                                 ref={ password2Ref } // Ensure this is correctly assigned
                             />
-                            <small></small>
+                            <div className='mb-6'>
+                                <small ref={ colorRef }></small>
+                            </div>
                         </div>
                         <center>
                             <button type="submit" className="bg-green-500 text-white px-20 py-2 rounded-md hover:bg-green-600">Login</button>
