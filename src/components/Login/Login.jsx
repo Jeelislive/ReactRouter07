40abc9976df85e0e726626c746b8c165667466eb
JEEL RUPARELIYA <jeelrupareliya255@gmail.com>
2024-02-27 22:35:21 +0530

    .

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -80,8 +80,8 @@ function Login() {
 
     const mystyle = {
         backgroundImage: "url('https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-        height: "100%",
-        width: "100%",
+        height: "100vh",
+        width: "100vw",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
     }
