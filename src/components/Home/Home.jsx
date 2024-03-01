d1ddf20d8e8d92b6842f94038a075bbbfdc20ce5
JEEL RUPARELIYA <jeelrupareliya255@gmail.com>
2024-03-01 14:46:49 +0530

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,16 +12,19 @@ export default function Home() {
                         </h2>
                     </div>
                 </div>
-                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img  className="w-96 rounded-lg drop-shadow-xl" alt="image1" />
+                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full">
+                    <img className="" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrs4cQN4JwWxQaFpf-wt1YRPy5rUk6uIiyrQ&usqp=CAU' alt="image1" style={ { width: '40vw', height: '25vw' } } />
                 </div>
+
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://blog.stucred.com/wp-content/uploads/2020/07/Study-area.jpg" alt="image2" />
+                <img className="sm:w-130 w-100" src="https://blog.stucred.com/wp-content/uploads/2020/07/Study-area.jpg" alt="image2" />
             </div>
 
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Let's Collaborate to create New</h1>
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium" >
+                <span style={ { color: '#DC2626' } }>Let's</span> Collaborate to create New
+            </h1>
         </div>
     );
 }
